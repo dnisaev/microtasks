5ebe9f63fbbf1643befd184826c6f7d9deee9332
dnisaev <dnisaev@yandex.ru>
2023-12-05 17:47:49 +0300

    change beforeStartSecondSprint

---
+++ src/components/copyBeforeStartSecondSprint/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Input} from "./components/inputLesson/Input";
+
+
+
+function App() {
+
+    return (
+        <Input/>
+    );
+}
+
+export default App;
